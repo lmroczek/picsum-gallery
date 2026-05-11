b4f3c62582b0402a85c27e086c6a8bc149f3279c
Logan Mroczek <lmroczek@reviveourhearts.com>
2026-05-11 13:31:39 -0400

    fetch images from picsum

---
+++ components/ImageGallery.tsx
@@ -0,0 +1,43 @@
+import { useEffect, useState } from "react";
+
+interface PicsumImage {
+  id: string;
+  author: string;
+  width: number;
+  height: number;
+  url: string;
+  download_url: string;
+}
+
+const ImageGallery = () => {
+  const [images, setImages] = useState<PicsumImage[]>([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState<string | null>(null);
+
+  useEffect(() => {
+    fetch("https://picsum.photos/v2/list")
+      .then((response) => response.json())
+      .then((data) => setImages(data))
+      .catch((error) => setError(error.message))
+      .finally(() => setLoading(false));
+  }, []);
+
+  if (loading) {
+    return <div>Loading...</div>;
+  }
+  if (error) {
+    return <div>Error: {error}</div>;
+  }
+
+  return (
+    <div>
+      {images.map((image) => (
+        <div key={image.id}>
+          <img src={image.download_url} alt={image.author} />
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default ImageGallery;
--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,14 @@
+import ImageGallery from "../components/ImageGallery.tsx";
+
 function App() {
   return (
     <div>
       <header>
         <h1 className="text-3xl font-bold text-center">Picsum Image Gallery</h1>
       </header>
+      <main>
+        <ImageGallery />
+      </main>
     </div>
   );
 }
