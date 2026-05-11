f6793518e30c099b83afb2dece4202bc4363624e
Logan Mroczek <lmroczek@reviveourhearts.com>
2026-05-11 11:56:01 -0400

    install tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,11 @@
-
 function App() {
   return (
-    <h1> Picsum Image Gallery </h1>
-  )
+    <div>
+      <header>
+        <h1 className="text-3xl font-bold text-center">Picsum Image Gallery</h1>
+      </header>
+    </div>
+  );
 }
 
 export default App;
