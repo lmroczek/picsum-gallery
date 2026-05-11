c63b324e4a055d63676fdb59391b7f3500a60436
Logan Mroczek <lmroczek@reviveourhearts.com>
2026-05-11 16:42:23 -0400

    clean up detail styling

--- src/components/ImageDetail.tsx
+++ src/components/ImageDetail.tsx
@@ -28,8 +28,8 @@ const ImageDetail = () => {
 
   return (
     <div>
-      <Link className="text-blue-400 pl-4" to="/">Back to Gallery</Link>
-      <h1 className="text-center">Photo by {image.author}</h1>
+      <Link className="text-blue-600 hover:underline pl-4" to="/">← Back to Gallery</Link>
+      <h1 className="mb-4 text-center">Photo by {image.author}</h1>
       <img
         className="mx-auto block max-h-[min(80vh,800px)] w-full max-w-5xl object-contain rounded-lg"
         src={image.download_url}
